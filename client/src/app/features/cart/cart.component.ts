867952b321b5d4594f12ca6186ae1016610530ec
DarkoJohnny13 <darkotadic724@gmail.com>
2025-02-02 19:32:44 +0100

    End of section 15

--- client/src/app/features/cart/cart.component.ts
+++ client/src/app/features/cart/cart.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { CartItemComponent } from './cart-item/cart-item.component';
 import { OrderSummaryComponent } from '../../shared/components/order-summary/order-summary.component';
+import { EmptyStateComponent } from '../../shared/components/empty-state/empty-state.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartItemComponent, OrderSummaryComponent],
+  imports: [CartItemComponent, OrderSummaryComponent, EmptyStateComponent],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
   ],
